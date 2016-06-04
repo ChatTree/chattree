c17e69a4eada23e4b19c41da8a784ffc1acaeba5
Steve Tseng <stevetseng@live.com>
2016-06-04 15:35:02 -0700

    home button works

--- client/js/main.js
+++ client/js/main.js
@@ -11,7 +11,7 @@ let App = React.createClass({
   render: function() {
     return (
       <div className="container">
-      	<Display pollInterval={3000} url="http://slack-server.elasticbeanstalk.com/messages"/>
+      	<Display pollInterval={3000} url="localhost:3000/"/>
       </div> 
     );
   }
