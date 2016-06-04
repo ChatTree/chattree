b146915fc97f9c4e68263b7eb2fb0e64239e6182
Steve Tseng <stevetseng@live.com>
2016-06-04 12:03:18 -0700

    front end ready

--- client/js/main.js
+++ client/js/main.js
@@ -11,7 +11,7 @@ let App = React.createClass({
   render: function() {
     return (
       <div className="container">
-      	<Display url="http://slack-server.elasticbeanstalk.com/messages"/>
+      	<Display pollInterval={3000} url="http://slack-server.elasticbeanstalk.com/messages"/>
       </div> 
     );
   }
