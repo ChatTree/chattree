4c414f17e73b80e962bc5e1ed3109b06ade4bdfd
jayunani <j.ayunani@gmail.com>
2016-06-04 11:42:48 -0700

    database set up

--- server/messages/messageController.js
+++ server/messages/messageController.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var Message = require('./messageModel');
+
+module.exports = {
+	getMessage: function() {
+
+	},
+	postMessage: function() {
+		
+	}
+}
--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ const http = require('http');
 const messageController = require('./messages/messageController');
 const server = http.createServer(app);
 const path = require('path');
+const data = require('./database/controller/dbController');
 
 app.use(express.static(path.join(__dirname + '/../client/')));
 app.get('/', function(req, res){
 	res.render('../client/index.html');
 });
+app.get('/database', function(req, res) {
+	data();
+})
 
 
 
